0c3515079d2e9ec1ca5aa8b74a9081cacf84d056
Zachary Yetman Van Egmond <zacharyyve@gmail.com>
2023-01-12 21:54:38 -0500

    Cleaned up linear models and seperated coefficients into seperate script

---
+++ pada_aero_coefficients.m
@@ -0,0 +1,45 @@
+% Stability & Control derivatives
+
+% Lift coefficients in stability frame
+C_L0 = 0;
+C_Lalpha = 0;
+C_Lde = 0;
+C_Lq = 0;
+% Drag coefficients in stability frame
+C_D0 = 0;
+C_Dalpha = 0;
+C_Dde = 0;
+C_Dq = 0;
+% Sidesslip force coeffients in Y-axis
+C_Y0 = 0;
+C_Ybeta = 0;
+C_Yda = 0;
+C_Yp = 0;
+C_Yr = 0;
+
+% Angular velocity x-axis
+C_pp = 0;
+C_pr = 0;
+C_p0 = 0;
+C_pbeta = 0;
+C_pda = 0;
+% Angular velocity z-axis
+C_rp = 0;
+C_rr = 0;
+C_r0 = 0;
+C_rbeta = 0;
+C_rda = 0;
+
+% Roll moment coefficients
+C_l0 = 0;
+C_lbeta = 0;
+C_lda = 0;
+% Pitch moment coefficients
+C_m0 = 0;
+C_malpha = 0;
+C_mde = 0;
+C_mq = 0;
+% Yaw moment coefficients
+C_n0 = 0;
+C_nbeta = 0;
+C_nda = 0;
--- pada_plant.m
+++ pada_plant.m
@@ -15,33 +15,8 @@ Ref_span = 1;
 Ref_length = 1;
 Ref_chord = 1;
 
-% Stability & Control derivatives
-% Lift
-C_L0 = 0;
-C_Lalpha = 0;
-C_Lde = 0;
-
-% Drag
-C_D0 = 0;
-C_Dalpha = 0;
-C_Dde = 0;
-
-% Side force
-C_y0 = 0;
-C_ybeta = 0;
-C_yda = 0;
-
-% Roll moment
-C_l0 = 0;
-C_lbeta = 0;
-C_lda = 0;
-
-% Pitch moment
-C_m0 = 0;
-C_malpha = 0;
-C_mde = 0;
-
-% Yaw moment
-C_n0 = 0;
-C_nbeta = 0;
-C_nda = 0;
+% Moments of inertia
+J_x = 1;
+J_y = 1;
+J_z = 1;
+J_xz = 1;
