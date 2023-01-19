0b5983f8fd04db2abb9d3c4b0329151c8f14fa2d
zacharyyve <zacharyyve@gmail.com>
2023-01-18 20:26:10 -0500

    Updated linear models + coefficients

--- pada_aero_coefficients.m
+++ pada_aero_coefficients.m
@@ -1,45 +1,32 @@
 % Stability & Control derivatives
 
 % Lift coefficients in stability frame
-C_L0 = 0;
-C_Lalpha = 0;
+C_L0 = 0.0193;
+C_Lalpha = 0.056;
 C_Lde = 0;
-C_Lq = 0;
+C_Lq = 3.48391;
 % Drag coefficients in stability frame
-C_D0 = 0;
+C_D0 = 0.0159;
 C_Dalpha = 0;
 C_Dde = 0;
 C_Dq = 0;
 % Sidesslip force coeffients in Y-axis
 C_Y0 = 0;
-C_Ybeta = 0;
+C_Ybeta = -0.6377;
 C_Yda = 0;
-C_Yp = 0;
-C_Yr = 0;
-
-% Angular velocity x-axis
-C_pp = 0;
-C_pr = 0;
-C_p0 = 0;
-C_pbeta = 0;
-C_pda = 0;
-% Angular velocity z-axis
-C_rp = 0;
-C_rr = 0;
-C_r0 = 0;
-C_rbeta = 0;
-C_rda = 0;
+C_Yp = -0.06787;
+C_Yr = 0.51996;
 
 % Roll moment coefficients
-C_l0 = 0;
-C_lbeta = 0;
-C_lda = 0;
+C_l0 = 0.0033;
+C_lbeta = -0.04415;
+C_lda = 0.0025326;
 % Pitch moment coefficients
-C_m0 = 0;
-C_malpha = 0;
+C_m0 = 0.00045;
+C_malpha = -0.3787;
 C_mde = 0;
-C_mq = 0;
+C_mq = -1.26832;
 % Yaw moment coefficients
 C_n0 = 0;
-C_nbeta = 0;
+C_nbeta = 0.12637;
 C_nda = 0;
--- pada_plant.m
+++ pada_plant.m
@@ -1,22 +1,22 @@
 % PADA Plant Model
 
-m = 1; % Mass of aircraft [kg]
+m = 0.4536; % Mass of aircraft [kg]
 g = 9.81; % Gravitational acceleration [m/s^2]
-rho = 1;
+rho = 1.225; % Air density [kg/m^3]
 
 % Motor & Propulsion properties
 S_prop = 1;
-C_prop = 1;
 k_motor = 1;
 
 % Wing properties
-Ref_area = 1;
-Ref_span = 1;
-Ref_length = 1;
-Ref_chord = 1;
+Ref_area = 0.1039; % Wing area [m^2]
+Ref_span = 0.4597; % Wing span [m]
+Ref_chord = 0.2472; % Wing chord [m]
 
 % Moments of inertia
 J_x = 1;
 J_y = 1;
 J_z = 1;
 J_xz = 0.1;
+
+Ref_length = 1; 
